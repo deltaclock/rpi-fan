319310badc03d5fc570fa186d7e2f5f929cb3f87
deltaclock <24370162+deltaclock@users.noreply.github.com>
2020-01-08 22:53:11 +0200

    ready

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+const (
+	// MaxTemp is the limit at which the fan will start running
+	MaxTemp = 65
+	// INTERVAL defines how often to check the temp
+	INTERVAL = time.Second * 5
+)
+
 func main() {
 	err := rpio.Open()
 
@@ -16,14 +23,23 @@ func main() {
 
 	defer rpio.Close()
 	// https://gpiozero.readthedocs.io/en/stable/_images/pin_layout.svg
-	pin := rpio.Pin(4)
+	pin := rpio.Pin(4) // GPIO4
 	pin.Mode(rpio.Output)
 	pin.Write(rpio.Low)
 
 	for {
-		time.Sleep(time.Second * 2)
-		pin.Toggle()
-		fmt.Println(getTemp())
+		temp, err := getTemp()
+
+		if err != nil {
+			panic(err)
+		}
+		if temp >= MaxTemp {
+			pin.Write(rpio.High)
+		} else {
+			pin.Write(rpio.Low)
+		}
+		fmt.Printf("Temp is at %d C", temp)
+		time.Sleep(INTERVAL)
 	}
 
 }

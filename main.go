c5f787109654b0171b4811724b14c11ab38737c3
deltaclock <24370162+deltaclock@users.noreply.github.com>
2021-09-25 19:54:36 +0300

    cleanup

--- main.go
+++ main.go
@@ -1,18 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-const (
+var options struct {
 	// MaxTemp is the limit at which the fan will start running
-	MaxTemp = 50
-	// INTERVAL defines how often to check the temp
-	INTERVAL = time.Second * 5
-)
+	MaxTemp int
+	// TempFile is the temperature as its being reported by the hardware
+	TempFile string
+	// Interval defines how often to check the temp
+	Interval time.Duration
+}
+
+func init() {
+	flag.IntVar(&options.MaxTemp, "temp", 45, "start fan after this temperature has been reached")
+	flag.StringVar(&options.TempFile, "file", "/sys/class/thermal/thermal_zone0/temp", "path to the temp report")
+	flag.DurationVar(&options.Interval, "time", 10*time.Second, "how often to check the temp")
+}
+
+func getTemp(tempFile string) int {
+
+	file, err := os.Open(tempFile)
+
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't open file %s for read!", tempFile))
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 7)
+
+	count, err := file.Read(buffer)
+
+	if err != nil {
+		panic(err)
+	}
+
+	temp := string(buffer[:count-1])
+	tempC, err := strconv.Atoi(temp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	tempC /= 1000
+
+	return tempC
+
+}
 
 func main() {
 	err := rpio.Open()
@@ -22,29 +64,29 @@ func main() {
 	}
 
 	defer rpio.Close()
+	defer fmt.Println("defer run!")
+
 	// https://gpiozero.readthedocs.io/en/stable/_images/pin_layout.svg
 	pin := rpio.Pin(4) // GPIO4
 	pin.Mode(rpio.Output)
 	pin.Write(rpio.Low)
+
 	spinning := false
 
-	for {
-		temp, err := getTemp()
+	c := time.Tick(options.Interval)
 
-		if err != nil {
-			panic(err)
-		}
+	for range c {
+		temp := getTemp(options.TempFile)
 
-		if temp >= MaxTemp && !spinning {
+		if temp >= options.MaxTemp && !spinning {
 			pin.Write(rpio.High)
 			spinning = true
-		} else if temp < MaxTemp && spinning {
+		} else if temp < options.MaxTemp && spinning {
 			pin.Write(rpio.Low)
 			spinning = false
 		}
 
 		fmt.Printf("Temp is at %d C\n", temp)
-		time.Sleep(INTERVAL)
 	}
 
 }
